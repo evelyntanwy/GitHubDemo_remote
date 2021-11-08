040f3054845d6f850ee65eb59a04617cea46ce99
Evelyn Tan <evetan.wy@gmail.com>
2021-11-08 15:47:34 +1300

    Create secondview

---
+++ GitHubDemo/Secondview.swift
@@ -0,0 +1,20 @@
+//
+//  Secondview.swift
+//  GitHubDemo
+//
+//  Created by Evelyn Tan on 8/11/21.
+//
+
+import SwiftUI
+
+struct Secondview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Secondview_Previews: PreviewProvider {
+    static var previews: some View {
+        Secondview()
+    }
+}
