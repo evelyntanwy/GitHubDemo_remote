16767b483d08563f6b7696700e89295e966fd168
Evelyn Tan <evetan.wy@gmail.com>
2021-11-08 15:37:02 +1300

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Evelyn Tan on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
